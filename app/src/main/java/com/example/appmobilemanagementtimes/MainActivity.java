2e08ad43f52866aaa24a94035ba0d34b729ff4a1
lequanghuy19204 <lequanghuy19204@gmail.com>
2025-04-01 01:38:58 +0700

    first commit

--- app/src/main/java/com/example/appmobilemanagementtimes/MainActivity.java
+++ app/src/main/java/com/example/appmobilemanagementtimes/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,6 +21,25 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.navigation_home) {
+                // Xử lý khi chọn Home
+                return true;
+            } else if (itemId == R.id.navigation_upcoming) {
+                // Xử lý khi chọn Upcoming
+                return true;
+            } else if (itemId == R.id.navigation_pomo) {
+                // Xử lý khi chọn Pomo
+                return true;
+            } else if (itemId == R.id.navigation_statistic) {
+                // Xử lý khi chọn Statistic
+                return true;
+            }
+            return false;
         });
     }
 }
